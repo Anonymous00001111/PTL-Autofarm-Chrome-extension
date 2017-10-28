2a8a5ede59937f090f86b42c6efcd945be0f9b31
Anonymous00001111 <33180264+Anonymous00001111@users.noreply.github.com>
2017-10-28 14:57:50 +0000

    Create inject.js

---
+++ inject.js
@@ -0,0 +1,37 @@
+    (function(){
+        var div = document.createElement('div');
+        div.style.position = 'fixed';
+        div.style.top = 0;
+        div.style.right = 0;
+        div.textContent = 'Injected!';
+        div.id = "injec";
+        document.body.appendChild(div);
+    
+        
+        function sleep(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+          }
+        console.log('Content script loaded and started');
+        setInterval(doStuff, 5 * 60 * 1000)
+        async function doStuff()
+        {   
+        try {
+            await sleep(Math.random() *500000);
+            console.log('Content script still loaded');
+            var time = document.getElementById("countdown-payday").innerText;
+            var a = parseInt(time);
+            if (a <= 0) {
+                var node = document.querySelector('[title="Payday auszahlen"]');
+                if (node == null)
+                {
+                    location.reload();
+                }
+                node.click();
+                await sleep(Math.random() *10000);
+            }
+        }
+        catch (e) {
+            location.reload();  
+        }
+        }
+}())
