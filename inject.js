641100e64f708bd4b47ee2d0254da39b55357719
Anonymous00001111 <33180264+Anonymous00001111@users.noreply.github.com>
2017-10-29 17:55:40 +0000

    V3
    Well we did it again :D

--- inject.js
+++ inject.js
@@ -1,16 +1,28 @@
     (function(){
-        var div = document.createElement('div');
+        var div = document.createElement('div');     
         div.style.position = 'fixed';
         div.style.top = 0;
         div.style.right = 0;
         div.textContent = 'Injected!';
         div.id = "injec";
         document.body.appendChild(div);
-        
+    
         
         function sleep(ms) {
             return new Promise(resolve => setTimeout(resolve, ms));
           }
+
+          Element.prototype.remove = function() {
+            this.parentElement.removeChild(this);
+        // }
+        NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+            for(var i = this.length - 1; i >= 0; i--) {
+                if(this[i] && this[i].parentElement) {
+                    this[i].parentElement.removeChild(this[i]);
+                }
+            }
+        }
+
         console.log('Content script loaded and started');
         setInterval(doStuff, 5 * 60 * 1000)
         async function doStuff()
@@ -22,20 +34,14 @@
             var a = parseInt(time);
             if (a <= 0) {
                 var node = document.querySelector('[title="Payday auszahlen"]');
-                var id_pd =document.getElementById("payday_payout");
-                var  tag_a = id_pd.getElementsByTagName('a');
-                if (tag_a != null && tag_a.length > 0) {
-                    var setLink = id_pd.parentNode.getElementsByTagName('a');
-                    if (setLink != null && setLink.length > 0) {
-                        setLink[0].href = tag_a[0].href;
-                    }
-                }    
                 if (node == null)
                 {
                     location.reload();
                 }
                 else{
-                    window.location.href = setLink[0].href;
+                node.remove();
+                var id_real = document.querySelector('[title="-> Payday auszahlen"]');
+                id_real.click();
                 }
             }
         }
